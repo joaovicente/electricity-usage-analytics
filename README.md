6ce81540cf8b26f1c6eab341df8e68f0449a890e
joaovicente <joao.diogo.vicente@gmail.com>
2023-04-10 14:18:03 +0100

    Docker image and initial httpbin.org test

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # electricity-usage-analytics
+
+
+## Run locally on windows
+```bash
+python electricity_usage_extraction.py -u bob@gmail.com -p mypassword -d 2023-04-06 --catchup
+```
+
+
+## Executing httpbin_collector in docker
+see https://github.com/umihico/docker-selenium-lambda and https://stackoverflow.com/questions/71746654/how-do-i-add-selenium-chromedriver-to-an-aws-lambda-function
+```sh
+# Build docker image https://stackify.com/docker-build-a-beginners-guide-to-building-docker-images/
+docker build -t joao/selenium-python .
+docker run -it --entrypoint /bin/bash joao/selenium-python
+python3 ./httpbin_collector.py
+```
---
+++ httpbin_collector.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import logging
+import os
+import time
+import platform
+from tempfile import mkdtemp
+
+os_name = platform.system()
+if os_name == 'Windows':
+    download_file_path = rf'C:\Users\{os.environ.get("USERNAME")}\Downloads'
+    chrome_driver_path = rf'C:\Users\{os.environ.get("USERNAME")}\Downloads\chromedriver'
+    service = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=service)
+else:
+    download_file_path = rf'/var/task'
+    chrome_driver_path = rf'/opt/chromedriver'
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    options.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome(chrome_driver_path, options=options)
+    
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+wait = WebDriverWait(driver, 30)
+url = 'https://httpbin.org/#/Response_formats/get_json'
+logging.info(f"Opening {url} page on {os_name}")
+driver.get(url)
+logging.info(f"Clicking on GET /json")
+wait.until(EC.visibility_of_element_located((By.ID, "operations-Response formats-get_json"))).click()
+logging.info(f"Clicking on Try out button")
+wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "try-out__btn"))).click()
+logging.info(f"Clicking on Execute button")
+wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "execute"))).click()
+logging.info(f"Clicking on Download button")
+wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "download-contents"))).click()
+
+time.sleep(5)
+downloaded_files = [f for f in os.listdir(download_file_path) if f.startswith("response")]
+if len(downloaded_files) > 0:
+    logging.info(f"{downloaded_files[0]} found in {download_file_path}")
+
+file_path = os.path.join(download_file_path, downloaded_files[0])
+logging.info(f"deleting {file_path}")
+os.remove(file_path)
+
+driver.close()
