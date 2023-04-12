3406ba92ceaaff865ab6fc4725e2ac25392ee2a2
joaovicente <joao.diogo.vicente@gmail.com>
2023-04-12 15:53:31 +0100

    updated tests

--- electricity_usage_collector_test.py
+++ electricity_usage_collector_test.py
@@ -25,13 +25,14 @@ from electricity_usage_collector import ElectricityUsageCollector, RuntimeMode, 
 #    row = "10305914213,31774820,0.174000,Active Import Interval (kW),06-04-2023 01:30"
 #    assert usage_row_to_date(row) == collection_datetime
     
-def mock_collection_data_as_list(start_datetime, end_datetime):
+def mock_collection_data_as_list(start_datetime, end_datetime, fixed_usage=True):
     data = []
     data.append('MPRN,Meter Serial Number,Read Value,Read Type,Read Date and End Time')
     row_datetime = start_datetime
-    usage = 0
+    usage = 1.000001
     while row_datetime <= end_datetime:
-        usage = usage + 0.000001
+        if not fixed_usage:
+            usage = usage + 0.000001
         row_datetime_as_string = row_datetime.strftime("%d-%m-%Y %H:%M")
         usage_as_string = "{:.6f}".format(usage)
         data.append(f"10305914213,31774820,{usage_as_string},Active Import Interval (kW),{row_datetime_as_string}")
@@ -39,25 +40,33 @@ def mock_collection_data_as_list(start_datetime, end_datetime):
     #print('\n'.join(data))
     return data
     
-def test_electricity_usage_collector():
+def test_electricity_usage_collector_first_collection():
     # First collection
-    collection_1 = mock_collection_data_as_list(
-        datetime(year=2023, month=1, day=1, hour=0, minute=0), 
-        datetime(year=2023, month=1, day=1, hour=1, minute=30))
-        #datetime(year=2023, month=1, day=2, hour=23, minute=30))
     collector = ElectricityUsageCollector("username", "password", RuntimeMode.TEST)
     collector.simulate_last_updated_datetime(None) # simulate no previous update
+    collection_1 = mock_collection_data_as_list(
+        datetime(year=2023, month=1, day=1, hour=0, minute=0), 
+        datetime(year=2023, month=1, day=2, hour=23, minute=30))
     collector.simulate_collection('\n'.join(collection_1)) # 2023-01-01 to 2023-01-02
     filename, csv_data = collector.persist_collected_data()
-    assert filename == "HDF-2023-01-01T0130.csv" # last persisted datetime
-    #assert filename == "HDF-2023-01-02T2330.csv" # last persisted datetime
-    csv_data_rows = csv_data.splitlines()
-    #raise ValueError(csv_data_rows)
-    #raise ValueError(collection_1)
-    assert csv_data_rows == collection_1
+    assert filename == "HDF-2023-01-02T2330.csv" # last persisted datetime
+    assert csv_data.splitlines() == collection_1
+    
+def test_electricity_usage_collector_second_collection():
     # second collection
-    #collector.simulate_latest_file_found(HDF-2023-01-02T2330.csv)
-    #...
+    collector = ElectricityUsageCollector("username", "password", RuntimeMode.TEST)
+     # simulate no previous update
+    collector.simulate_last_updated_datetime(datetime(year=2023, month=1, day=2, hour=23, minute=30))
+    collection_2 = mock_collection_data_as_list(
+        datetime(year=2023, month=1, day=1, hour=0, minute=0), 
+        datetime(year=2023, month=1, day=4, hour=23, minute=30))
+    collector.simulate_collection('\n'.join(collection_2)) # 2023-01-01 to 2023-01-04
+    filename, csv_data = collector.persist_collected_data()
+    expected_persisted_data = mock_collection_data_as_list(
+        datetime(year=2023, month=1, day=3, hour=0, minute=0), 
+        datetime(year=2023, month=1, day=4, hour=23, minute=30))
+    assert filename == "HDF-2023-01-04T2330.csv" # last persisted datetime
+    assert csv_data.splitlines() == expected_persisted_data
     
     
     
