7758c3c94728c5ec8b10ba437d81878e0693af05
joaovicente <joao.diogo.vicente@gmail.com>
2023-04-12 16:59:37 +0100

    Cleaned up tests

--- electricity_usage_collector_test.py
+++ electricity_usage_collector_test.py
@@ -1,30 +1,7 @@
 from datetime import datetime
 from datetime import timedelta
-from electricity_usage_collector import ElectricityUsageCollector, RuntimeMode, usage_row_filter, usage_row_to_date
+from electricity_usage_collector import ElectricityUsageCollector, RuntimeMode
 
-#def test_usage_row_filter():
-#    collection_datetime = datetime.strptime('2023-04-06', '%Y-%m-%d')
-#    input = """\
-#MPRN,Meter Serial Number,Read Value,Read Type,Read Date and End Time
-#10305914213,31774820,0.174000,Active Import Interval (kW),05-04-2023 01:30
-#10305914213,31774820,0.174000,Active Import Interval (kW),06-04-2023 01:30
-#10305914213,31774820,0.174000,Active Import Interval (kW),07-04-2023 01:30"""
-#    expected = """\
-#MPRN,Meter Serial Number,Read Value,Read Type,Read Date and End Time
-#10305914213,31774820,0.174000,Active Import Interval (kW),06-04-2023 01:30"""
-#    expected_catchup = """\
-#MPRN,Meter Serial Number,Read Value,Read Type,Read Date and End Time
-#10305914213,31774820,0.174000,Active Import Interval (kW),05-04-2023 01:30
-#10305914213,31774820,0.174000,Active Import Interval (kW),06-04-2023 01:30"""
-#    assert usage_row_filter(input, collection_datetime) == expected
-#    assert usage_row_filter(input, collection_datetime, catchup=True) == expected_catchup
-    
-    
-#def test_usage_row_to_date():
-#    collection_datetime = datetime.strptime('2023-04-06', '%Y-%m-%d')
-#    row = "10305914213,31774820,0.174000,Active Import Interval (kW),06-04-2023 01:30"
-#    assert usage_row_to_date(row) == collection_datetime
-    
 def mock_collection_data_as_list(start_datetime, end_datetime, fixed_usage=True):
     data = []
     data.append('MPRN,Meter Serial Number,Read Value,Read Type,Read Date and End Time')
